aee30f1e6c78e51ba6a30f7cd0d8524fb806e092
NazarBylen <nazar.bylen.ir.2021@lpnu.ua>
2022-05-07 14:02:00 +0300

    code cleared

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ from flask import Flask, render_template, jsonify, request, make_response, url_f
 
 app = Flask(__name__)
 
-
-@app.route('/GET/subject', methods=['GET'])
-def getting():
+def getData():
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, 'static', 'data.json')
     res = json.load(open(json_url, encoding='utf-8'))
+    return res
+
+res = getData()
+
+@app.route('/subject', methods=['GET'])
+def getting():
 
     return jsonify(res)
 
 
-@app.route('/POST/subject', methods=['POST'])
+@app.route('/subject', methods=['POST'])
 def posting():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'static', 'data.json')
-    res = json.load(open(json_url, encoding='utf-8'))
 
     res["data"].append({
         "id": 11,
@@ -29,11 +30,8 @@ def posting():
     return jsonify(res)
 
 
-@app.route('/PUT/subject/<int:id>', methods=['PUT'])
+@app.route('/subject/<int:id>', methods=['PUT'])
 def putting(id):
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'static', 'data.json')
-    res = json.load(open(json_url, encoding='utf-8'))
 
     res["data"][id - 1].update({
         "id": 34,
@@ -43,11 +41,8 @@ def putting(id):
     return jsonify(res)
 
 
-@app.route('/DELETE/subject/<int:id>', methods=['DELETE'])
+@app.route('/subject/<int:id>', methods=['DELETE'])
 def deleting(id):
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'static', 'data.json')
-    res = json.load(open(json_url, encoding='utf-8'))
 
     del res["data"][id - 1]
 
